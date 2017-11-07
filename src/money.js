08c2ab646bb9468536564a0e20299f8e8230b80b
cm-yamasaki-michihiro <yamasaki.michihiro@classmethod.jp>
2017-11-07 11:39:31 +0900

    chap-8

--- src/money.js
+++ src/money.js
@@ -1,6 +1,41 @@
 export default class Money {
+
     equals(other) {
         return this.constructor === other.constructor
           && this.amount === other.amount
     }
+
+    static dollar(amount) {
+        return new Dollar(amount)
+    }
+
+    static franc(amount) {
+        return new Franc(amount)
+    }
+}
+
+class Franc extends Money { 
+    
+    constructor(amount) { 
+        super()
+        this.amount = amount
+    }
+    
+    times(multiplier) { 
+        return new Franc(this.amount * multiplier)
+    }
+    
+}
+
+class Dollar extends Money { 
+    
+    constructor(amount) { 
+        super()
+        this.amount = amount
+    }
+    
+    times(multiplier) { 
+        return new Dollar(this.amount * multiplier)
+    }
+    
 }
